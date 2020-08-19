6d0b7237a85bc14b8932687dedaf9c0db1b2b434
Brenden Niedermeyer <contact@brenden.dev>
2020-08-18 19:34:48 -0700

    feat: add launch darkly

--- src/http/get-emotion/index.js
+++ src/http/get-emotion/index.js
@@ -1,10 +1,29 @@
 const getEmotionWithMicrosoftApi = require("./microsoft-vision");
+const LaunchDarkly = require("launchdarkly-node-server-sdk");
+const ldClient = LaunchDarkly.init(process.env.LD_API_KEY);
 
 exports.handler = async function http(req) {
   const { url } = req.queryStringParameters;
 
   try {
-    const emotion = await getEmotionWithMicrosoftApi(url);
+    await ldClient.waitForInitialization();
+    const visionProvider = await ldClient.variation(
+      "vision-api-provider",
+      { key: "contact@brenden.dev" },
+      false
+    );
+    // since this is an ephemeral function, release all resources
+    ldClient.close();
+
+    let emotion;
+
+    if (visionProvider === "Microsoft") {
+      emotion = await getEmotionWithMicrosoftApi(url);
+    } else if (visionProvider === "Google") {
+      console.log("SEND REQUEST TO GOOGLE ");
+      emotion = { emotion: "google" };
+    }
+
     console.log(emotion);
     return {
       headers: {
